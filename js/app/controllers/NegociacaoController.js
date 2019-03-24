67d8c2804f505a1311e90cd79eb1d253aafefda6
Diego Delmiro <diegoalbuquerque03@gmail.com>
2019-03-24 12:12:56 -0300

    A ligação entre as ações do usuário e o modelo.

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -2,17 +2,32 @@ class NegociacaoController{
     
     constructor(){
         let $ = document.querySelector.bind(document);
-        this.inputData = $('#data');
-        this.inputQuantidade = $('#quantidade');
-        this.inputValor = $('#valor');
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
     }
 
 
     adiciona(event){
         event.preventDefault();
-        console.log(this.inputData.value);
-        console.log(this.inputQuantidade.value);
-        console.log(this.inputValor.value);
+        
+        let data = new Date(...
+            this._inputData.value.split('-').
+            map(function(data, posicao){
+                if(posicao == 1){
+                    return data - 1;
+                }
+                return data;
+            })
+        );
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputData.value,
+            this._inputValor.value
+        );
+        console.log(data);
+        console.log(negociacao);
     }
 
 }
