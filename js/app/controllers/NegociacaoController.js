dba5c1d3390817464333964599dc3c4b81099772
Diego Delmiro <diegoalbuquerque03@gmail.com>
2019-03-24 21:48:42 -0300

    Criando ListModel para objeto Negociacao.

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -5,20 +5,31 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new ListaNegociacoes();
     }
 
 
     adiciona(event){
         event.preventDefault();     
-       
-        let negociacao = new Negociacao(
+        this._negociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+        console.log(this._negociacoes.getNegociacoes);    
+    }
+    
+    _limpaFormulario(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 0;
+        this._inputValor.value = 0.0;
+        
+        this._inputData.focus();
+    }
+    
+    _criaNegociacao(){
+        return new Negociacao(
             DateHelper.textParseData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
-            );     
-
-        console.log(negociacao);
-        console.log(DateHelper.dataParseText(negociacao.data));    
+        );     
     }
 
 }
---
+++ js/app/models/ListaNegociacoes.js
@@ -0,0 +1,17 @@
+class ListaNegociacoes{
+
+    constructor(){
+        this._negociacoes = [];
+    }   
+
+    adiciona(negociacao){
+        
+        this._negociacoes.push(negociacao);
+    }
+
+    get getNegociacoes(){
+        
+        return this._negociacoes;
+    }
+
+}
